ea123dd42fd78b88f13590cb7057f8fc0ec7bc70
guoning <guoning@national-space.com>
2022-12-04 16:52:43 +0800

    feat:20221204

--- back/data_manage/run.sh
+++ back/data_manage/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker stop data_manage
 docker rm data_manage
-docker-compose -f ../../compose/docker-compose.yml up -d data_manage
+docker-compose -f ../../compose/docker-compose.yml up -d --compatibility data_manage
 
